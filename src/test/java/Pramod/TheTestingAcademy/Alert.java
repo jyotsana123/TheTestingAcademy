18402e0f712cac933a9053a29b7a3e6650d2f135
jyotsana pandey <jyotsana.branddirect@gmail.com>
2023-08-12 16:35:55 +0530

    changes

---
+++ src/test/java/Pramod/TheTestingAcademy/Alert.java
@@ -0,0 +1,16 @@
+package Pramod.TheTestingAcademy;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Alert {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://the-internet.herokuapp.com/javascript_alerts");
+
+	}
+
+}
